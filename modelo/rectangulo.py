5c965dbafc654448f8e9e94baf8132ab7f9571c9
Juankaprez <jc.perezl@uea.edu.ec>
2026-01-11 13:35:27 -0500

    Add Rectangulo class with area calculation method

---
+++ modelo/rectangulo.py
@@ -0,0 +1,7 @@
+class Rectangulo:
+    def __init__(self, base: float, altura: float):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self) -> float:
+        return self.base * self.altura
